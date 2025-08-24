867ac3d0050f0bc29069d7e127c2bf861f58beef
Devin <dolsen1@pkglobal.com>
2025-08-23 21:44:26 -0700

    Removeal of lua files

--- source/Corners.lua
+++
@@ -1,5 +0,0 @@
-local Corners = {}
-
-function Corners.make(p, r) local c=Instance.new("UICorner"); c.CornerRadius=UDim.new(0,r or 8); c.Parent=p; return c end
-
-return Corners
--- source/Strokes.lua
+++
@@ -1,5 +0,0 @@
-local Strokes = {}
-
-function Strokes.make(p, c, t) local s=Instance.new("UIStroke"); s.Color=c; s.Thickness=t or 1; s.Parent=p; return s end
-
-return Strokes
